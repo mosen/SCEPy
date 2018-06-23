2108556a7c743ab5a64c611a08870aaf3e5c9abd
Mosen <mosen@users.noreply.github.com>
2018-06-23 20:28:44 +1000

    move the /mobileconfig test endpoint into the blueprint

--- scepy/__init__.py
+++ scepy/__init__.py
@@ -1,5 +1,3 @@
-from flask import Flask, url_for
-import plistlib
 from .ca import CertificateAuthority
 from .storage import FileStorage
 from .message import SCEPMessage
@@ -42,39 +40,3 @@ app.config.from_envvar('SCEPY_SETTINGS', True)
 app.wsgi_app = WSGIChunkedBodyCopy(app.wsgi_app)
 # app.register_blueprint(admin_app)
 app.register_blueprint(scep_app)
-
-
-@app.route('/mobileconfig')
-def mobileconfig():
-    """Quick and dirty SCEP enrollment mobileconfiguration profile."""
-    my_url = url_for('scep_app.scep', _external=True)
-
-    profile = {
-        'PayloadType': 'Configuration',
-        'PayloadDisplayName': 'SCEPy Enrolment Test Profile',
-        'PayloadDescription': 'This profile will enroll your device with the SCEP server',
-        'PayloadVersion': 1,
-        'PayloadIdentifier': 'com.github.mosen.scepy',
-        'PayloadUUID': '7F165A7B-FACE-4A6E-8B56-CA3CC2E9D0BF',
-        'PayloadContent': [
-            {
-                'PayloadType': 'com.apple.security.scep',
-                'PayloadVersion': 1,
-                'PayloadIdentifier': 'com.github.mosen.scepy.scep',
-                'PayloadUUID': '16D129CA-DA22-4749-82D5-A28201622555',
-                'PayloadDisplayName': 'SCEPy Test Enrolment Payload',
-                'PayloadDescription': 'SCEPy Test Enrolment Payload',
-                'PayloadContent': {
-                    'URL': my_url,
-                    'Name': 'SCEPY',
-                    'Keysize': 2048,
-                    'Key Usage': 5
-                }
-            }
-        ]
-    }
-
-    if 'SCEPY_CHALLENGE' in app.config:
-        profile['PayloadContent'][0]['PayloadContent']['Challenge'] = app.config['SCEPY_CHALLENGE']
-
-    return plistlib.dumps(profile), {'Content-Type': 'application/x-apple-aspen-config'}
--- scepy/blueprint.py
+++ scepy/blueprint.py
@@ -5,7 +5,8 @@ from cryptography.hazmat.primitives.serialization import Encoding
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from asn1crypto.csr import CertificationRequestInfo
-from flask import Blueprint, abort, g, current_app, request, Response
+from flask import Blueprint, abort, g, current_app, request, Response, url_for
+import plistlib
 
 from .message import SCEPMessage
 from .ca import CertificateAuthority
@@ -176,4 +177,40 @@ def scep():
             return ''
     else:
         abort(404, 'unknown SCEP operation')
+
+
+@scep_app.route('/mobileconfig')
+def mobileconfig():
+    """Quick and dirty SCEP enrollment mobileconfiguration profile."""
+    my_url = url_for('scep_app.scep', _external=True)
+
+    profile = {
+        'PayloadType': 'Configuration',
+        'PayloadDisplayName': 'SCEPy Enrolment Test Profile',
+        'PayloadDescription': 'This profile will enroll your device with the SCEP server',
+        'PayloadVersion': 1,
+        'PayloadIdentifier': 'com.github.cmdmnt.scepy',
+        'PayloadUUID': '7F165A7B-FACE-4A6E-8B56-CA3CC2E9D0BF',
+        'PayloadContent': [
+            {
+                'PayloadType': 'com.apple.security.scep',
+                'PayloadVersion': 1,
+                'PayloadIdentifier': 'com.github.cmdmnt.scepy.scep',
+                'PayloadUUID': '16D129CA-DA22-4749-82D5-A28201622555',
+                'PayloadDisplayName': 'SCEPy Test Enrolment Payload',
+                'PayloadDescription': 'SCEPy Test Enrolment Payload',
+                'PayloadContent': {
+                    'URL': my_url,
+                    'Name': 'SCEPY',
+                    'Keysize': 2048,
+                    'Key Usage': 5
+                }
+            }
+        ]
+    }
+
+    if 'SCEPY_CHALLENGE' in current_app.config:
+        profile['PayloadContent'][0]['PayloadContent']['Challenge'] = current_app.config['SCEPY_CHALLENGE']
+
+    return plistlib.dumps(profile), {'Content-Type': 'application/x-apple-aspen-config'}
 
